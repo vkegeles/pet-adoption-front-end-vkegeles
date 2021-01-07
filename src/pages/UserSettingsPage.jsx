a419cf09ab0cc35e68ffbb2a4c9b0a907343ef6b
Violetta Kegeles <46904779+vkegeles@users.noreply.github.com>
2021-01-07 22:24:27 +0200

    fix warnings

--- src/pages/UserSettingsPage.jsx
+++ src/pages/UserSettingsPage.jsx
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function UserSettingsPage() {
-  const { user, getUser } = useAuth();
+  const { getUser } = useAuth();
   const history = useHistory();
   const classes = useStyles();
 
