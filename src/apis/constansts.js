62c5248a024c69d0d19ba5f4863b890f5b26bf9d
Violetta Kegeles <46904779+vkegeles@users.noreply.github.com>
2021-01-07 21:57:15 +0200

    deploing

--- src/apis/constansts.js
+++ src/apis/constansts.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const URL = "http://localhost:5000";
-export const baseURL = "http://localhost:5000";
+export const URL = "https://pet-adoption-back-end-vkegeles.herokuapp.com";
+export const baseURL = "https://pet-adoption-back-end-vkegeles.herokuapp.com";
 export const my_app = axios.create({ baseURL });
 export const CLOUD_NAME = "vkegeles";
 export const ADMIN_STATUS = 2;
